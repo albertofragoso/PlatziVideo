f17ce3ca985a7c2d288d7ef852fad0d5a7f02cb8
albertofragoso <hola@albertofragoso.com>
2019-09-15 21:19:28 -0500

    Adding images with Webpack.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import '../assets/styles/components/Header.scss'
+import logo from '../assets/static/logo.png'
+import userIcon from '../assets/static/user.png'
 
 const Header = () => (
   <header className="header">
-    <img tabIndex="0" className="header__img" src="./logo.png" alt="Platzi Video" />
+    <img tabIndex="0" className="header__img" src={logo} alt="Platzi Video" />
     <div className="header__menu">
       <div className="header__menu--profile">
-        <img src="./user.png" alt="User image" />
+        <img src={userIcon} alt="User image" />
         <p>Profile</p>
       </div>
       <ul>
