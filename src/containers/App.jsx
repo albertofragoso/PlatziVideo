be9d41b7e1e371e26aba04e21bad28e845d0597f
albertofragoso <hola@albertofragoso.com>
2019-09-15 20:38:36 -0500

    Creating Carousel component.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
+import Carousel from '../components/Carousel'
 
 import '../assets/styles/App.scss'
 
@@ -9,7 +10,9 @@ const App = () => (
   <div className="App">
     <Header />
     <Search />
-    <Categories />
+    <Categories>
+      <Carousel />
+    </Categories>
   </div>
 )
 
