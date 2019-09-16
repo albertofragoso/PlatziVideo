bf5672aeda1b8d7d39e3428c31d4439f9cd8ee8d
albertofragoso <hola@albertofragoso.com>
2019-09-15 19:48:49 -0500

    Creating App container.

--- src/components/HelloWorld.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import '../assets/styles/App.scss'
-
-const HelloWorld = () => 'Hello React!'
-
-export default HelloWorld
---
+++ src/containers/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+const App = () => (
+  <div className="App"></div>
+)
